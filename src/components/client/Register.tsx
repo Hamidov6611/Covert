9dcd0d6da540d615f43ec93ec5f5f2c8cda00651
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-09-11 15:25:32 +0500

    Register

--- src/components/client/Register.tsx
+++ src/components/client/Register.tsx
@@ -1,8 +1,8 @@
-
 import axios from "axios"
 import Cookies from 'js-cookie';
 import { Checkbox } from "@mui/material";
 import React, { FC, useState } from "react";
+import { useTranslation } from "react-i18next";
 
 interface propsRegister {
   setIsRegister: (value: boolean) => void;
@@ -14,7 +14,7 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
   const [checkedproccess2, setCheckedProccess2] = useState<boolean>(false);
   const [username, setUserName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  
+  const [t] = useTranslation("global");
   const handleOnchange1 = (e: React.ChangeEvent<HTMLInputElement>) =>
     setUserName(e.target.value);
   const handleOnchange2 = (e: React.ChangeEvent<HTMLInputElement>) =>
@@ -36,20 +36,16 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
   }
 
 
-
-
   return (
     <div
       onClick={() => setIsRegister(false)}
       className="fixed top-0 left-0 flex justify-center items-center bg-modalBg z-[999] w-[100%] h-[100%]"
     >
       <div
-      
         onClick={(e) => e.stopPropagation()}
-        className="w-[90%] sm:w-[70%] lg:w-[36%] overflow-y-auto min-h-[77vh] bg-white flex flex-col p-3 sm:p-6 rounded-lg"
+        className="w-[90%] sm:w-[70%] lg:w-[36%] overflow-y-auto min-h-[70vh] bg-white flex flex-col p-3 sm:p-6 rounded-lg"
       >
         <form onSubmit={postData}>
-
         <div className="flex w-[100%] justify-end">
           <div
             onClick={() => setIsRegister(false)}
@@ -66,32 +62,32 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
         </div>
         <div className="my-2 sm:my-4">
           <p className="font-semibold text-[15px] sm:text-[24px] md:text-[26px] lg:text-[30px] text-lightGreey font-montserrat">
-            Зарегистрируйтесь в качестве клиента
+            {t(`registration.title`)}
           </p>
         </div>
         <div className="mb-2 mt-2 sm:mt-3 sm:mb-3 flex flex-col">
           <p className="mb-2 text-[#344054] font-medium text-[15px] sm:text-[21px]  font-montserrat">
-          Логин
+          {t(`registration.ui1`)}
           </p>
           <input
             value={username}
             onChange={handleOnchange1}
             type="text"
             className="border w-[100%] py-2 rounded-md text-[#667085] text-[14px] px-3 focus:outline-[#1348F9] font-medium"
-            placeholder="Введите логин"
+            placeholder={t(`registration.pl1`)}
           />
         </div>
 
         <div className="mb-2 mt-2 sm:mt-3 sm:mb-3 flex flex-col">
           <p className="mb-2 text-[#344054] font-medium text-[15px] sm:text-[21px]  font-montserrat">
-          Пароль
+          {t(`registration.ui2`)}
           </p>
           <input
             value={password}
             onChange={handleOnchange2}
             type="password"
             className="border w-[100%] py-2 rounded-md text-[#667085] text-[14px] px-3 focus:outline-[#1348F9] font-medium"
-            placeholder="Введите свой пароль"
+            placeholder={t(`registration.pl2`)}
           />
         </div>
 
@@ -104,8 +100,7 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
           </div>
           <div className="w-[100%] sm:w-[66%]">
             <p className="ml-3 sm:ml-0 font-medium text-lightGreey text-[12px] sm:text-[17px]  font-montserrat">
-              Я принимаю положения Условий предоставления услуг для
-              пользователей
+            {t(`registration.ask1`)}
             </p>
           </div>
         </div>
@@ -119,28 +114,25 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
           </div>
           <div className="w-[100%] sm:w-[66%]">
             <p className="ml-3 sm:ml-0 font-medium text-lightGreey text-[12px] sm:text-[17px] font-montserrat">
-              Я согласен получать электронные письма о предложении UK2EU{" "}
+            {t(`registration.ask2`)}
             </p>
           </div>
         </div>
 
         <p className="mt-6 font-medium text-lightGreey text-[12px] sm:text-[17px] font-montserrat">
-          UK2EU обрабатывает данные для выполнения контракта, юридических
-          обязанностей и содержания согласий. У вас есть право получать доступ,
-          исправлять, стирать, ограничивать обработку, переносимость данных,
-          отзывать согласие и возражать. Политика конфиденциальности
+        {t(`registration.text`)}
         </p>
 
         <div className="flex mt-2 sm:mt-4">
           <p className="font-montserrat text-[14px] text-lightGreey mr-5">
-            У тебя есть аккаунт?
+          {t(`registration.ask3`)}
           </p>
           <button
           type="button"
             onClick={handleClick}
             className=" font-montserrat text-[14px] text-blue-700"
           >
-            Вход
+            {t(`registration.res`)}
           </button>
         </div>
 
@@ -149,7 +141,7 @@ const Register: FC<propsRegister> = ({ setIsRegister, setIsLogin }) => {
             type="submit"
             className="md:mt-4 bg-heroPrimary hover:bg-Primary transition duration-200 ease-in-out text-white font-semibold text-[17px] sm:text-[18px] py-1 md:py-3 px-8 md:px-24 rounded-md"
           >
-            Регистрация
+            {t(`registration.btn`)}
           </button>
         </div>
         </form>
