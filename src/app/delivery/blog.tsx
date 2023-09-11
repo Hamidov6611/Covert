ef293eaa543e03cabc96788a4deddc359ac1679e
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-09-11 14:01:58 +0500

    Blog

--- src/app/delivery/blog.tsx
+++ src/app/delivery/blog.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import RootLayout from "../../components/layouts/RootLayout";
+import { useTranslation } from "react-i18next";
 
 const Blog = () => {
+  const [t] = useTranslation("global");
   return (
     <RootLayout title="БЛОГ">
       <div className="py-4 md:py-16 md:w-[90%] w-[96%] mx-auto ">
         <p className="font-semibold text-lightGreey text-center text-[22px] sm:text-[28px] md:text-[35px] lg:text-[65px] ">
-          Наш Блог
+          {t(`blog.title`)}
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-8 pt-8">
           <div className="min-h-[540px] sm:min-h-[590px] relative border rounded-md shadow-xl">
@@ -24,16 +26,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
@@ -55,16 +56,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
@@ -86,16 +86,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
@@ -117,16 +116,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
@@ -148,16 +146,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
@@ -179,16 +176,15 @@ const Blog = () => {
                 22.06.2023
               </p>
               <p className="mt-4 font-semibold text-[25px] text-lightGreey">
-                Пять способов снизить транспортные расходы
+                {t(`blog.card.title`)}
               </p>
               <p className="text-[15.99px] text-lightGreey font-light mt-2 line-clamp-3">
-                Существуют много советов которым мы можем легко следовать ,
-                чтобы снизить транспортные расходы
+                {t(`blog.card.text`)}
               </p>
               <div className="absolute bottom-6 left-6">
                 <Link to={"/blog/1"}>
                   <p className="text-[#1348F9] text-[17px] font-semibold border-2 p-2 rounded-md hover:bg-[#1348F9] hover:text-[#fff] hover:border-transparent transition duration-500 ease-in-out border-[#1348F9]">
-                    Читать больше
+                    {t(`blog.card.btn`)}
                   </p>
                 </Link>
               </div>
