dc460a8cde5ce6e11c3cbdee1693a9b2f0a5949f
Hamidov6611 <dilmuroddelitot@gmail.com>
2023-09-19 16:41:43 +0500

    UserLayout

---
+++ src/components/layouts/UserLayout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { UserNavbar, UserSidebar } from "../user";
+import UserFooter from "../user/UserFooter";
+
+type UserLayoutProps = {
+  children: React.ReactNode;
+  title?: string;
+  desc?: string;
+};
+
+const UserLayout = ({ children, title, desc }: UserLayoutProps) => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta name="description" content={desc} />
+      </Helmet>
+
+      <UserNavbar />
+
+      <main className="w-[100%] flex flex-row justify-between">
+        <div className="w-[0%] md:w-[15%]">
+          <UserSidebar />
+        </div>
+        <div className="w-[100%] md:w-[85%] bg-[#F6F6F6]">{children}</div>
+      </main>
+      <footer className="md:ml-[15%] w-full md:w-[85%] min-h-[300px] bg-[#0F172A]">
+        <UserFooter />
+      </footer>
+    </>
+  );
+};
+
+UserLayout.defaultProps = {
+  title: "User",
+};
+
+export default UserLayout;
